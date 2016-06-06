16e0131904032fe82ee71289a7ecad95577031d5
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2016-06-06 19:56:20 +0000

    Use parameter from URL as search keyword

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ const express = require('express'),
       https = require('https'),
       portNumber = process.env.PORT || process.argv[2] || 8080;
 
-https.get(`https://www.googleapis.com/customsearch/v1?q=love&key=${process.env.GOOGLE_CUSTOM_SEARCH_KEY}&cx=${process.env.cx}`, function (res) {
-  res.setEncoding("utf8");
-  let json = "";
-  res
-  .on('data', function (chunk) {
-    json += chunk;
-  })
-  .on('end', function () {
-    json = JSON.parse(json);
-    console.log(json.items);
+app.get('/api/:keyword', function (req, res) {
+  https.get(`https://www.googleapis.com/customsearch/v1?q=${req.params.keyword}&key=${process.env.GOOGLE_CUSTOM_SEARCH_KEY}&cx=${process.env.cx}`, function (result) {
+    result.setEncoding("utf8");
+    let json = "";
+    result
+    .on('data', function (chunk) {
+      json += chunk;
+    })
+    .on('end', function () {
+      json = JSON.parse(json);
+      res.send(json.items);
+    });
   });
 });
 
