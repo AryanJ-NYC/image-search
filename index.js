34744e7acb2f03c210c52c48ce44ce8e5ef57bf8
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2016-06-06 01:24:22 -0400

    Get and log custom google search

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 "use strict"
+require('dotenv').config();
 const express = require('express'),
       app = express(),
+      https = require('https'),
       portNumber = process.env.PORT || process.argv[2] || 8080;
+
+https.get(`https://www.googleapis.com/customsearch/v1?q=love&key=${process.env.GOOGLE_CUSTOM_SEARCH_KEY}&cx=${process.env.cx}`, function (res) {
+  res.setEncoding("utf8");
+  let json = "";
+  res
+  .on('data', function (chunk) {
+    json += chunk;
+  })
+  .on('end', function () {
+    json = JSON.parse(json);
+    console.log(json.items);
+  });
+});
 
 app.listen(portNumber, function () {
   console.log("Listening on port " + portNumber);
